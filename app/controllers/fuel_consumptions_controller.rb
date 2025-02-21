9601f3fa7c14c5a7e38be73d76f050df9a144d49
lukas-rdtk <lukas-rdtk@proton.me>
2025-02-21 22:42:47 +0100

    WEBAPP-3 edit record

--- app/controllers/fuel_consumptions_controller.rb
+++ app/controllers/fuel_consumptions_controller.rb
@@ -1,4 +1,6 @@
 class FuelConsumptionsController < ApplicationController
+  before_action :set_fuel_consumption, only: [:edit, :update, :destroy]
+
   def index
     @fuel_consumptions = FuelConsumption.all
   end
@@ -10,21 +12,35 @@ class FuelConsumptionsController < ApplicationController
   def create
     @fuel_consumption = FuelConsumption.new(fuel_consumption_params)
     if @fuel_consumption.save
-      flash[:success] = 'Fuel record created successfully.'
-      redirect_to fuel_consumptions_path
+      redirect_to fuel_consumptions_path, notice: 'Fuel consumption record was successfully created.'
     else
       render :new
     end
   end
 
+  def edit
+    @fuel_consumption = FuelConsumption.find(params[:id])
+  end
+
+  def update
+    @fuel_consumption = FuelConsumption.find(params[:id])
+    if @fuel_consumption.update(fuel_consumption_params)
+      redirect_to fuel_consumptions_path, notice: 'Fuel consumption record was successfully updated.'
+    else
+      render :edit
+    end
+  end
+
   def destroy
-    fuel_consumption = FuelConsumption.find(params[:id])
-    fuel_consumption.destroy
-    flash[:success] = "Fuel record deleted successfully."
-    redirect_to fuel_consumptions_path
+    @fuel_consumption.destroy
+    redirect_to fuel_consumptions_path, notice: 'Fuel consumption record was successfully deleted.'
   end
 
   private
+
+  def set_fuel_consumption
+    @fuel_consumption = FuelConsumption.find(params[:id])
+  end
 
   def fuel_consumption_params
     params.require(:fuel_consumption).permit(:car, :liters, :price, :refuel_date, :distance, :avg_consumption)
