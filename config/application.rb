e2d364ec7c4f05ede75824a46b426872569c7c1a
lukas-rdtk <lukas-rdtk@proton.me>
2025-06-27 11:46:47 +0200

    WEBAPP-5 Upgrade Rails to version 8.0 and update dependencies
    The changes involve upgrading Rails to version 8.0.0 and making the
    necessary adjustments to configuration files, templates, and
    dependencies to ensure compatibility.

--- config/application.rb
+++ config/application.rb
@@ -9,10 +9,8 @@ Bundler.require(*Rails.groups)
 module Webapp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 7.0
+    config.load_defaults 8.0
 
-    config.i18n.default_locale = :de
-    
     # Please, add to the `ignore` list any other `lib` subdirectories that do
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
